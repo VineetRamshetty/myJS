b88faea418849b70eb2158d50e434944a682ab8b
Vineet Ramshetty <vineet.ramshetty@gmail.com>
2026-01-07 14:09:10 +0530

    l5 uploaded

---
+++ l5/aq1/app.js
@@ -0,0 +1,19 @@
+console.log(Math.floor(Math.random()*6)+1);
+
+let car={
+    name: "Rolls Royals",
+    model: "Phantom (Series VIII)",
+    color: "black"
+};
+console.log(car.name);
+
+let person={
+    name: "Vineet",
+    age: 20,
+    city: "Hyderabad",
+};
+console.log(person);
+person.city="New Yort";
+console.log(person);
+person.country="USA";
+console.log(person);
